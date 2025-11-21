5ee9b2a119b95f80d0ad705896cc5d188ee9c686
Bernd Porr <mail@berndporr.me.uk>
2025-11-21 10:11:25 +0000

    Using libtorch now.

---
+++ dnf.cpp
@@ -0,0 +1,34 @@
+#include "dnf.h"
+
+float DNF::filter(const float signal, const float noise) {
+    signal_delayLine.push_back(signal);
+    const float delayed_signal = signal_delayLine[0];
+    
+    for (int i = noiseDelayLineLength-1 ; i > 0; i--) {
+	noise_delayLine[i] = noise_delayLine[i-1];
+    }
+    noise_delayLine[0] = noise;
+
+    // NOISE INPUT TO NETWORK
+    torch::Tensor data = torch::from_blob(
+        noise_delayLine, {noiseDelayLineLength}, torch::kFloat);
+
+    // REMOVER OUTPUT FROM NETWORK
+    torch::Tensor output;
+
+    output = model->forward(data);
+    
+    auto a = output.accessor<float,1>();
+    remover = a[0];
+    
+    f_nn = delayed_signal - remover;
+    
+    // FEEDBACK TO THE NETWORK
+    optimizer->zero_grad();
+    torch::Tensor gradient = torch::tensor({f_nn,f_nn});
+    output.retain_grad();
+    output.backward(gradient);
+    optimizer->step();
+
+    return f_nn;
+}
--- dnf.h
+++ dnf.h
@@ -1,11 +1,13 @@
 #ifndef _DNF_H
 #define _DNF_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+#include <torch/torch.h>
+#include <thread>
 #include <boost/circular_buffer.hpp>
-
-#include "dnf/Neuron.h"
-#include "dnf/Layer.h"
-#include "dnf/Net.h"
 
 /**
  * Main Deep Neuronal Network main class.
@@ -14,150 +16,160 @@
  **/
 class DNF {
 public:
-	/**
-	 * Constructor which sets up the delay lines, network layers
-	 * and also calculates the number of neurons per layer so
-	 * that the final layer always just has one neuron.
-	 * \param NLAYERS Number of layers
-	 * \param numTaps Number of taps for the delay line feeding into the 1st layer
-	 * \param fs Sampling rate of the signals used in Hz.
-	 * \param am The activation function for the neurons. Default is tanh.
-	 **/
-	DNF(const int NLAYERS,
-	    const int numTaps,
-	    const double fs,
-	    const Neuron::actMethod am = Neuron::Act_Tanh,
-	    const bool debugOutput = false
-		) : noiseDelayLineLength(numTaps),
-		    signalDelayLineLength(noiseDelayLineLength / 2),
-		    signal_delayLine(signalDelayLineLength),
-		    nNeurons(new int[NLAYERS]),
-		    noise_delayLine(new double[noiseDelayLineLength]()) {		
-		// calc an exp reduction of the numbers always reaching 1
-		double b = exp(log(noiseDelayLineLength)/(NLAYERS-1));
-		for(int i=0;i<NLAYERS;i++) {
-			nNeurons[i] = ceil(noiseDelayLineLength / pow(b,i));
-			if (i == (NLAYERS-1)) nNeurons[i] = 1;
-		}
-		
-		//create the neural network
-		NNO = new Net(NLAYERS, nNeurons, noiseDelayLineLength, 0, "");
-		
-		//setting up the neural networks
-		for(int i=0;i<NLAYERS;i++) {
-			NNO->getLayer(i)->initLayer(i,Neuron::W_RANDOM, Neuron::B_NONE, am);
-			if (debugOutput) {
-				fprintf(stderr,"Layer %d has %d neurons. act = %d\n",i,nNeurons[i],am);
-			}
-		}
+
+    /**
+     * Options for method of initialising weights
+     * 0 for initialising all weights to zero
+     * 1 for initialising all weights to one
+     * 2 for initialising all weights to a random value between 0 and 1
+     */
+    enum WeightInitMethod { W_ZEROS = 0,
+	W_ONES = 1, W_RANDOM = 2, W_ONES_NORM = 3, W_RANDOM_NORM = 5,  };
+    
+    /**
+     * Options for activation functions of the neuron
+     * 0 for using the logistic function
+     * 1 for using the hyperbolic tan function
+     * 2 for unity function (no activation)
+     */
+    enum ActMethod {Act_Sigmoid = 1, Act_Tanh = 2, Act_ReLU = 3, Act_NONE = 0};
+
+    struct Net : public torch::nn::Module {
+	std::vector<torch::nn::Linear> fc;
+	
+	Net(int nLayers, int nInput, bool debug=false) {
+	    // calc an exp reduction of the numbers always reaching 1
+	    const float b = (float)exp(log(nInput)/(nLayers-1));
+	    int inputNeurons = nInput;
+	    for(int i=1;i<nLayers;i++) {
+		const int outputNeurons = (int)ceil(nInput / pow(b,i));
+		char tmp[256];
+		if (debug)
+		    sprintf(tmp,"fc%d_%d_%d",i,inputNeurons,outputNeurons);
+		fprintf(stderr,"Creating FC layer: %s\n",tmp);
+		fc.push_back(register_module(tmp, torch::nn::Linear(inputNeurons, outputNeurons)));
+		inputNeurons = outputNeurons;
+	    }
+	    if (debug)
+		fprintf(stderr,"Creating FC output layer: %d -> output\n",inputNeurons);
+	    fc.push_back(register_module("fc_out", torch::nn::Linear(inputNeurons, 2)));
 	}
 
-	enum ErrorPropagation { Backprop = 0, ModulatedHebb = 1 };
-
-	void setErrorPropagation(const ErrorPropagation e) {
-		errorPropagation = e;
+	torch::Tensor forward(torch::Tensor x) {
+	    for(auto& f:fc) {
+		x = torch::atan(f->forward(x));
+	    }
+	    return x;
 	}
 
-	/**
-	 * Realtime sample by sample filtering operation
-	 * \param signal The signal contaminated with noise. Should be less than one.
-	 * \param noise The reference noise. Should be less than one.
-	 * \returns The filtered signal where the noise has been removed by the DNF.
-	 **/
-	double filter(const double signal, const double noise) {
-		signal_delayLine.push_back(signal);
-		const double delayed_signal = signal_delayLine[0];
-		
-		for (int i = noiseDelayLineLength-1 ; i > 0; i--) {
-			noise_delayLine[i] = noise_delayLine[i-1];
-		}
-		noise_delayLine[0] = noise / (double)noiseDelayLineLength;
+    };
 
-		// NOISE INPUT TO NETWORK
-		NNO->setInputs(noise_delayLine);
-		NNO->propInputs();
-		
-		// REMOVER OUTPUT FROM NETWORK
-		remover = NNO->getOutput(0);
-		f_nn = delayed_signal - remover;
-		
-		// FEEDBACK TO THE NETWORK 
-		NNO->setError(f_nn);
-		switch (errorPropagation) {
-		case Backprop:
-		default:
-			NNO->propErrorBackward();
-			break;
-		case ModulatedHebb:
-			NNO->propModulatedHebb(f_nn);
-			break;
-		}
-		NNO->updateWeights();
-		return f_nn;
+    /**
+     * Constructor which sets up the delay lines, network layers
+     * and also calculates the number of neurons per layer so
+     * that the final layer always just has one neuron.
+     * \param nLayers Number of layers
+     * \param nTaps Number of taps for the delay line feeding into the 1st layer
+     * \param fs Sampling rate of the signals used in Hz.
+     * \param am The activation function for the neurons. Default is tanh.
+     **/
+    DNF(const int nLayers,
+	const int nTaps,
+	const float fs,
+	const ActMethod am = Act_Tanh,
+	const bool debugOutput = false
+	) : noiseDelayLineLength(nTaps),
+	    signalDelayLineLength(noiseDelayLineLength / 2),
+	    signal_delayLine(signalDelayLineLength),
+	    noise_delayLine(new float[noiseDelayLineLength]()) {
+
+	torch::manual_seed(1);
+    
+	torch::DeviceType device_type;
+	if (torch::cuda::is_available()) {
+	    std::cout << "CUDA available! Training on GPU." << std::endl;
+	    device_type = torch::kCUDA;
+	} else {
+	    std::cout << "Training on CPU." << std::endl;
+	    device_type = torch::kCPU;
 	}
+	torch::Device device(device_type);
 
-	/**
-	 * Returns a reference to the whole neural network.
-	 * \returns A reference to Net.
-	 **/
-	inline Net& getNet() const {
-		return *NNO;
-	}
+	model = new Net(nLayers,nTaps,debugOutput);
+	model->to(device);
+	model->train();
+	
+	optimizer = new torch::optim::SGD(model->parameters(), 0);
+    }
 
-	/**
-	 * Returns the length of the delay line which
-	 * delays the signal polluted with noise.
-	 * \returns Number of delay steps in samples.
-	 **/
-	inline const int getSignalDelaySteps() const {
-		return signalDelayLineLength;
-	}
+    void setLearningRate(float mu) {
+	for (auto& group : optimizer->param_groups()) {
+            static_cast<torch::optim::SGDOptions&>(group.options()).lr(mu);
+        }
+    }
 
-	/**
-	 * Returns the delayed with noise polluted signal by the delay 
-	 * indicated by getSignalDelaySteps().
-	 * \returns The delayed noise polluted signal sample.
-	 **/
-	inline const double getDelayedSignal() const {
-		return signal_delayLine[0];
-	}
+    /**
+     * Realtime sample by sample filtering operation
+     * \param signal The signal contaminated with noise. Should be less than one.
+     * \param noise The reference noise. Should be less than one.
+     * \returns The filtered signal where the noise has been removed by the DNF.
+     **/
+    float filter(const float signal, const float noise);
 
-	/**
-	 * Returns the remover signal.
-	 * \returns The current remover signal sample.
-	 **/
-	inline const double getRemover() const {
-		return remover;
-	}
-
-	/**
-	 * Returns the output of the DNF: the the noise
-	 * free signal.
-	 * \returns The current output of the DNF which is idential to filter().
-	 **/
-	inline const double getOutput() const {
-		return f_nn;
-	}
-
-	/**
-	 * Frees the memory used by the DNF.
-	 **/
-	~DNF() {
-		delete NNO;
-		delete[] nNeurons;
-		delete[] noise_delayLine;
-	}
-
+    /**
+     * Returns the length of the delay line which
+     * delays the signal polluted with noise.
+     * \returns Number of delay steps in samples.
+     **/
+    inline int getSignalDelaySteps() const {
+	return signalDelayLineLength;
+    }
+    
+    /**
+     * Returns the delayed with noise polluted signal by the delay 
+     * indicated by getSignalDelaySteps().
+     * \returns The delayed noise polluted signal sample.
+     **/
+    inline float getDelayedSignal() const {
+	return signal_delayLine[0];
+    }
+    
+    /**
+     * Returns the remover signal.
+     * \returns The current remover signal sample.
+     **/
+    inline float getRemover() const {
+	return remover;
+    }
+    
+    /**
+     * Returns the output of the DNF: the the noise
+     * free signal.
+     * \returns The current output of the DNF which is idential to filter().
+     **/
+    inline float getOutput() const {
+	return f_nn;
+    }
+    
+    /**
+     * Frees the memory used by the DNF.
+     **/
+    ~DNF() {
+	delete optimizer;
+	delete model;
+    }
+    
 private:
-	Net *NNO;
-	int noiseDelayLineLength;
-	int signalDelayLineLength;
-	boost::circular_buffer<double> signal_delayLine;
-	double* noise_delayLine;
-	int* nNeurons;
-	double remover = 0;
-	double f_nn = 0;
-	ErrorPropagation errorPropagation = Backprop;
+    Net* model;
+    torch::Tensor output;
+    torch::optim::SGD* optimizer = nullptr;
+    int noiseDelayLineLength;
+    int signalDelayLineLength;
+    boost::circular_buffer<float> signal_delayLine;
+    float* noise_delayLine;
+    int* nNeurons;
+    float remover = 0;
+    float f_nn = 0;
 };
 
 #endif
