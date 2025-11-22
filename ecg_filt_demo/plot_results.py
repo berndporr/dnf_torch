ea1520b6a927e79ade0fcce42e85d9027bad28f7
Bernd Porr <mail@berndporr.me.uk>
2025-11-22 22:57:58 +0000

    Made the plot more meaningful.

--- ecg_filt_demo/plot_results.py
+++ ecg_filt_demo/plot_results.py
@@ -6,17 +6,24 @@ fs = 1000
 
 data = np.loadtxt("ecg_filtered.dat")
 
-d0 = data[:,0]
-d1 = data[:,1]
-d2 = data[:,2]
+filtered = data[:,0]
+orig = data[:,1]
+remover = data[:,2]
+
+t = np.linspace(0,1/fs*len(orig),len(orig))
 
 fig=plt.figure()
 ax1 = plt.subplot(311)
 ax2 = plt.subplot(312, sharex = ax1)
 ax3 = plt.subplot(313, sharex = ax1)
-ax1.plot(d0)
-ax2.plot(d1)
-ax3.plot(d2)
-plt.xlabel("time")
-plt.ylabel("volt")
-plt.show()    
+ax1.plot(t,orig)
+ax1.set_title("Original signal")
+ax1.set_ylabel("Orig/V")
+ax2.plot(t,remover)
+ax2.set_title("Remover")
+ax2.set_ylabel("Rem/V")
+ax3.plot(t,filtered)
+ax3.set_title("Filtered")
+ax3.set_ylabel("Filt/V")
+plt.xlabel("time/sec")
+plt.show()
